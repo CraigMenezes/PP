1f3ac99faac18d82d9ee75b9f416353bcf424562
Craig Banks <craigbanks@Craigs-MacBook-Air.local>
2019-09-16 12:29:59 +0530

    made user schema & validated user entry

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-//@route  GET api/users
-//@desc   test route
+//@route  POST api/users
+//@desc   register user
 //@access public
-router.get('/',(req,res)=>res.send("user route"));
+router.post(
+  "/",
+  [
+    check("name", "Please enter a name")
+      .not()
+      .isEmpty(),
+    check("email", "please enter a valid email").isEmail(),
+    check(
+      "password",
+      "please enter a password with more than 6 characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors= validationResult(req);
+    if(!errors.isEmpty()){
+       return res.status(400).json({errors:errors.array()});
+    }
+    res.send("user route");
+  }
+);
 
-module.exports=router;
+module.exports = router;
