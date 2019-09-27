d4f4fc1ddd85dc85f5f0325ef2b3dc184c5bedbe
CraigMenezes <craigpmenezes@gmail.com>
2019-09-27 18:51:46 +0530

    register/login part of registration form completed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
-import React, { Fragment,useState } from "react";
+import React, { Fragment, useState } from "react";
+// import axios from 'axios';
+import {Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,18 +13,38 @@ const Register = () => {
   const { name, email, password, password2 } = formData;
 
   const onChange = e =>
-    setFormData({
+    setFormData({ 
       ...formData,
       [e.target.name]: e.target.value
     });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log("passwords dont match");
-    }else{
-        console.log(formData);
+    } else {
+        console.log('Success');
         
+    //   const newUser = {
+    //     name,
+    //     email,
+    //     password
+    //   };
+
+    //   try {
+    //       const config={
+    //           headers:{
+    //               'Content-Type':'application/json'
+    //           }
+    //       }
+
+    //       const body= JSON.stringify(newUser)
+
+    //       const res =await axios.post('/api/users', body, config);
+    //       console.log(res.data);
+    //   } catch (error) {
+    //       console.error(error.response.data);
+    //   }
     }
   };
 
@@ -82,7 +104,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
